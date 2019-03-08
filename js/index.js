89388be13b49c1030dce8729d27d2e763b87a0ed
Rudy Ravelin <rudy@noukod.com>
2019-03-08 17:45:42 +0000

    Automatically backed up by Learn

---
+++ js/index.js
@@ -0,0 +1,24 @@
+class Event {
+  constructor(title, keywords) {
+    this.title = title;
+    this.keywords = keywords;
+  }
+}
+
+class User {
+  constructor(name, interests) {
+    this.name = name;
+    this.interests = interests;
+  }
+
+  matchInterests(event) {
+    return event.keywords.some(word => this.interests.includes(word));
+  }
+
+}
+
+let billy = new User('billy', ['music', 'art', 'movies']);
+let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
+
+billy.matchInterests(freeMusic);
+;
